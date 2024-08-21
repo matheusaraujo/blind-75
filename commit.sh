62e3bd18a56c73b42b344d6ef2ac7e952b8c515f
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-21 18:18:41 +0200

    chore: commit script

---
+++ commit.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+
+TOPICS=(
+  "arrays-hashing"
+  "two-pointers"
+  "sliding-window"
+  "stack"
+  "binary-search"
+  "linked-list"
+  "trees"
+  "heap"
+  "backtracking"
+  "tries"
+  "graphs"
+  "advanced-graphs"
+  "1d-dynamic-programming"
+  "2d-dynamic-programming"
+  "greedy"
+  "intervals"
+  "math-geometry"
+  "bit-manipulation"
+)
+
+is_valid_topic() {
+  local topic=$1
+  for valid_topic in "${TOPICS[@]}"; do
+    if [ "$topic" == "$valid_topic" ]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
+if [ "$#" -ne 1 ]; then
+  printf "%susage: %s %s\n" "$RED" "$0" "$RESET"
+  exit 1
+fi
+
+TOPIC="$1"
+
+if ! is_valid_topic "$TOPIC"; then
+  printf "%sinvalid git topic%s\n" "$RED" "$RESET"
+  exit 1
+fi
+
+NEW_FILES=$(git status --short | grep -E '\.py$' | awk '{print $2}' | sed '/^$/d')
+FILE_COUNT=$(echo "$NEW_FILES" | sed '/^$/d' | wc -l | tr -d ' ')
+
+if [ "$FILE_COUNT" -ne 1 ]; then
+  printf "%sinvalid git status%s\n" "$RED" "$RESET"
+  exit 1
+fi
+
+FILE_NAME=$(echo "$NEW_FILES" | head -n 1)
+COMMIT_MESSAGE="feat($TOPIC): $(basename "$FILE_NAME")"
+
+git add "$FILE_NAME"
+git commit -m "$COMMIT_MESSAGE"
+
+printf "%scommitted%s\n" "$GREEN" "$RESET"
