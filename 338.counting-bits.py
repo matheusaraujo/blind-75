cd2a7863993a6b396f1a20af3b13a1be407abb7b
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-21 18:30:22 +0200

    feat(bit-manipulation): 338.counting-bits.py

---
+++ 338.counting-bits.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=338 lang=python3
+#
+# [338] Counting Bits
+#
+
+# @lc code=start
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i: offset = i
+            dp[i] = 1 + dp[i - offset]
+
+        return dp
+
+# @lc code=end
+
