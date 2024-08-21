f79b5499544569927ba064573c34665f15271164
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-21 17:38:20 +0200

    feat(two-pointers): 125.valid-palindrome.py

---
+++ 125.valid-palindrome.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+
+        def isValidChar(c: str):
+            return c.isalnum()
+
+        while i < j and j >= 0 and i < len(s):
+            while i < j and not isValidChar(s[i]):
+                i += 1
+            while j > i and not isValidChar(s[j]):
+                j -= 1
+
+            if s[i].casefold() != s[j].casefold():
+                return False
+            i, j = i + 1, j - 1
+
+        return True
+
+# @lc code=end
+
