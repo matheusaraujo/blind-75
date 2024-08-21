c0e9a6a92134e02adf25a57db01d80a3e33ae603
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-21 23:25:09 +0200

    refactor(commit): defining functions

--- commit.sh
+++ commit.sh
@@ -35,35 +35,57 @@ is_valid_topic() {
   return 1
 }
 
-if [ "$#" -ne 1 ]; then
-  printf "%susage: %s %s\n" "$RED" "$0" "$RESET"
-  exit 1
-fi
+get_new_files() {
+  git status --short | grep -E '\.py$' | awk '{print $2}' | sed '/^$/d'
+}
 
-TOPIC="$1"
+validate_file_count() {
+  local file_count
+  file_count=$(echo "$1" | wc -l | tr -d ' ')
+  if [ "$file_count" -ne 1 ]; then
+    printf "%sinvalid git status%s\n" "$RED" "$RESET"
+    exit 1
+  fi
+}
 
-if ! is_valid_topic "$TOPIC"; then
-  printf "%sinvalid topic%s\n" "$RED" "$RESET"
-  exit 1
-fi
+build_commit_message() {
+  local topic=$1
+  local file_name=$2
 
-NEW_FILES=$(git status --short | grep -E '\.py$' | awk '{print $2}' | sed '/^$/d')
-FILE_COUNT=$(echo "$NEW_FILES" | sed '/^$/d' | wc -l | tr -d ' ')
+  if [[ "$file_name" == extras/* ]]; then
+    echo "feat($topic) [extras]: $(basename "$file_name")"
+  else
+    echo "feat($topic): $(basename "$file_name")"
+  fi
+}
 
-if [ "$FILE_COUNT" -ne 1 ]; then
-  printf "%sinvalid git status%s\n" "$RED" "$RESET"
-  exit 1
-fi
+main() {
+  if [ "$#" -ne 1 ]; then
+    printf "%susage: %s %s\n" "$RED" "$0" "$RESET"
+    exit 1
+  fi
 
-FILE_NAME=$(echo "$NEW_FILES" | head -n 1)
+  local topic="$1"
 
-if [[ "$FILE_NAME" == extras/* ]]; then
-  COMMIT_MESSAGE="feat($TOPIC): $(basename "$FILE_NAME")  [extras]"
-else
-  COMMIT_MESSAGE="feat($TOPIC): $(basename "$FILE_NAME")"
-fi
+  if ! is_valid_topic "$topic"; then
+    printf "%sinvalid topic%s\n" "$RED" "$RESET"
+    exit 1
+  fi
 
-git add "$FILE_NAME"
-git commit -m "$COMMIT_MESSAGE"
+  local new_files
+  local file_name
+  local commit_message
 
-printf "%scommitted%s\n" "$GREEN" "$RESET"
+  new_files=$(get_new_files)
+  validate_file_count "$new_files"
+
+  file_name=$(echo "$new_files" | head -n 1)
+  commit_message=$(build_commit_message "$topic" "$file_name")
+
+  git add "$file_name"
+  git commit -m "$commit_message"
+
+  printf "%scommitted%s\n" "$GREEN" "$RESET"
+}
+
+main "$@"
