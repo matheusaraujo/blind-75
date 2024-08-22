f2415f837d9b815bcbac31f5ec776ff2266874a7
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-22 20:46:01 +0200

    feat(graphs): 133.clone-graph.py

---
+++ 133.clone-graph.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=133 lang=python3
+#
+# [133] Clone Graph
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        mapping = {}
+        def clone(node: Node):
+            if node in mapping: return mapping[node]
+
+            copy = Node(node.val)
+            mapping[node] = copy
+            for neighbor in node.neighbors:
+                copy.neighbors.append(clone(neighbor))
+            return copy
+
+        return clone(node) if node else None
+
+# @lc code=end
+
