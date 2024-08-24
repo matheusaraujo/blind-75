84fb9305a559a226b95c7c79ae6739f9750ae343
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-24 19:36:02 +0200

    feat(intervals): 57.insert-interval.py

---
+++ 57.insert-interval.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=57 lang=python3
+#
+# [57] Insert Interval
+#
+
+# @lc code=start
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                result.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1])
+                ]
+        result.append(newInterval)
+        return result
+
+
+
+# @lc code=end
+
