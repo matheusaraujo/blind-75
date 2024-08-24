a17dfd7b96a955ffc1019fcf38b53f1763960570
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-25 00:17:32 +0200

    feat(1d-dynamic-programming): 322.coin-change.py

---
+++ 322.coin-change.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=322 lang=python3
+#
+# [322] Coin Change
+#
+
+# @lc code=start
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] + [math.inf] * (amount + 1)
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+
+        return dp[amount] if dp[amount] != math.inf else -1
+
+
+
+# @lc code=end
+
