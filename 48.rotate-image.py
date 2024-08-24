48ac83efebc44fe89d03c4571462bcf8ba1ee81c
Matheus Araujo <matheus.saraujo@gmail.com>
2024-08-24 20:41:20 +0200

    feat(math-geometry): 48.rotate-image.py

---
+++ 48.rotate-image.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=48 lang=python3
+#
+# [48] Rotate Image
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        left, right = 0, len(matrix) - 1
+
+        while left < right:
+            for i in range(right - left):
+                top, bottom = left, right
+
+                topLeft = matrix[top][left + i]
+                matrix[top][left + i] = matrix[bottom - i][left]
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                matrix[bottom][right - i] = matrix[top + i][right]
+                matrix[top + i][right] = topLeft
+            right -= 1
+            left += 1
+
+# @lc code=end
+
